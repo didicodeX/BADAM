85b503e745b584a1a2bcc1e5b2835a8bd9b41073
Dylane <tanolamene@gmail.com>
2025-04-27 03:07:33 -0300

    feat: integrate routing and loading states with React Router and add loading screen component

---
+++ src/app/SmartHomeRedirect.jsx
@@ -0,0 +1,14 @@
+// import { Navigate } from "react-router-dom";
+// import { useAuthStore } from "@/shared/store/auth.store";
+
+// export default function SmartHomeRedirect() {
+//   const { user } = useAuthStore();
+
+//   if (user) {
+//     // Si connecté ➔ aller vers HomePage privée
+//     return <Navigate to="/home" replace />;
+//   }
+
+//   // Sinon ➔ LandingPage publique
+//   return <Navigate to="/landing" replace />;
+// }
---
+++ src/features/landing/pages/LandingPage.jsx
@@ -0,0 +1,8 @@
+// LandingPage.jsx
+export default function LandingPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Bienvenue sur BADAM 🎉</h1>
+    </div>
+  );
+}
