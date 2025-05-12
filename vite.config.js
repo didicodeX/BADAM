bdec6e9e21670cbb8b7174efe10560ca0bda20f4
Dylane <tanolamene@gmail.com>
2025-05-12 02:42:35 -0300

    feat: Update Vite configuration to include path aliasing and ESM support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from "vite-tsconfig-paths"; 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+// Simuler __dirname pour ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
