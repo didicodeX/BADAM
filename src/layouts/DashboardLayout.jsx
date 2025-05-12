32c98f853019f83fb8035df56f0b535f6a4b7984
Dylane <tanolamene@gmail.com>
2025-05-12 05:33:28 -0300

    feat: Remove unused Breadcrumbs import from DashboardLayout

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -2,11 +2,9 @@ import DashboardNavbar from "@/features/dashboard/components/DashboardNavbar";
 import DashboardNavbarMobile from "@/features/dashboard/components/DashboardNavbarMobile";
 import DashboardSidebar from "@/features/dashboard/components/DashboardSidebar";
 import BottomNav from "@/features/dashboard/components/BottomNav";
-
 import { Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
 import ScrollToTop from "@/shared/components/ScrollToTop";
-import Breadcrumbs from "@/shared/components/Breadcrumbs";
 
 export default function DashboardLayout() {
   const [menuOpen, setMenuOpen] = useState(false);
